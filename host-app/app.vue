be107d6ccb53750e2edb3a44e82f9d785d465ce1
chirathvintrepid <chirath.vandabona@intrepidtravel.com>
2025-03-06 10:15:31 +0530

    Added remote and host app

---
+++ host-app/app.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="container mx-auto p-4 text-center">
+    <h1 class="text-2xl font-bold">Host App</h1>
+    <Suspense>
+      <RemoteComponent />
+    </Suspense>
+  </div>
+</template>
+
+<script setup>
+import { defineAsyncComponent } from "vue";
+
+const RemoteComponent = defineAsyncComponent(() =>
+  import("remoteApp/RemoteComponent")
+);
+</script>
+
+<style>
+.container {
+  text-align: center;
+  padding: 20px;
+}
+</style>
---
+++ remote-app/components/RemoteComponent.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="p-4 bg-blue-200 border border-blue-400 rounded-lg">
+    <h2 class="text-xl font-bold">Remote Component</h2>
+    <p>This component is from the Remote App!</p>
+  </div>
+</template>
+
+<script setup></script>
+
+<style scoped>
+div {
+  text-align: center;
+}
+</style>
